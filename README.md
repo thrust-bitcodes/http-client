4358f64c987c99c3e7d3659e11b05062025770b0
Bruno Machado Brandão <brunin5010@gmail.com>
2018-01-17 10:37:20 -0200

    Incluindo arquivos de configuração. Ajustando README e erro de retorno do fluent

--- README.md
+++ README.md
@@ -1,20 +1,43 @@
-# http-client
-BitCode que implementa métodos de acesso HTTP e HTTPS do lado do 'client'
+HttpClient
+===============
 
-## Example HTTP Client
+HttpClient é um *bitcode* de acesso HTTP e HTTPS para [ThrustJS](https://github.com/thrustjs/thrust).
+
+# Instalação
+
+Posicionado em um app [ThrustJS](https://github.com/thrustjs/thrust), no seu terminal:
+
+```bash
+thrust install http-client
+```
+
+## Tutorial
+
 ```javascript
-print ("\n",
-    HTTPClient
-        // .post("http://localhost:8080/test/pecho", {params: {nome: "P Paulo", idade: 13}})
-        .post("http://localhost:8080/test/pecho")
-        .params({params: {nome: "David", idade: 10}})
-        // .get("http://example.com")
-        // .get("http://localhost:8080/test/json")
-        // .get("http://localhost:8080/test/echo", {params: {nome: "P Paulo", idade: 13}})
-        // .get("http://localhost:8080/test/echo")
-        // .params({params: {nome: "P Paulo", idade: 13}})
-        .charset("UTF-8")
-        .fetch()
-    , "\n"
-)
+let httpClient = require('http-client')
+
+let result = httpClient
+  // .post("http://localhost:8080/test/pecho", {params: {nome: "P Paulo", idade: 13}})
+  .post("http://localhost:8080/test/pecho")
+  .params({params: {nome: "David", idade: 10}})
+  // .get("http://example.com")
+  // .get("http://localhost:8080/test/json")
+  // .get("http://localhost:8080/test/echo", {params: {nome: "P Paulo", idade: 13}})
+  // .get("http://localhost:8080/test/echo")
+  // .params({params: {nome: "P Paulo", idade: 13}})
+  .charset("UTF-8")
+  .contentType('application/json')
+  .property('prop', 'value')
+  .fetch()
+
+show(result)
+```
+
+O *bitcode* http-client contém os seguintes métodos, sendo que cada um deles retornam o mesmo *builder* conforme exemplo acima.
+
+```javascript
+get(url, params)
+post(url, params)
+put(url, params)
+delete(url, params)
 ```
--- index.js
+++ index.js
@@ -2,82 +2,81 @@ var Scanner = Java.type("java.util.Scanner")
 var URL = Java.type("java.net.URL")
 var StandardCharsets = Java.type("java.nio.charset.StandardCharsets")
 
-
 function mount_http_request(method, url, params) {
-    var request = this
-    var params = http.serializeParams(params || {})
-    var properties = {
-        charset: StandardCharsets.UTF_8,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
+  var request = this
+  var params = http.serializeParams(params || {})
+  var properties = {
+    charset: StandardCharsets.UTF_8,
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
 
-    var fluent = {
-        params: (function (pars) {
-            params = http.serializeParams(pars)
+  var fluent = {
+    params: (function (pars) {
+      params = http.serializeParams(pars)
 
-            return fluent
-        }).bind(request),
+      return fluent
+    }).bind(request),
 
-        property: (function (property, value) {
-            properties[property] = value
+    property: (function (property, value) {
+      properties[property] = value
 
-            return request
-        }).bind(request),
+      return fluent
+    }).bind(request),
 
-        charset: (function (value) {
-            properties["Accept-Charset"] = value
+    charset: (function (value) {
+      properties["Accept-Charset"] = value
 
-            return fluent
-        }).bind(request),
+      return fluent
+    }).bind(request),
 
-        contentType: (function (value) {
-            properties["Content-Type"] = value
+    contentType: (function (value) {
+      properties["Content-Type"] = value
 
-            return fluent
-        }).bind(request),
+      return fluent
+    }).bind(request),
 
-        get_content: function(http_connection) {
-            var inputStream = http_connection.getInputStream()
-            var scanner = new Scanner(inputStream, "UTF-8")
-            var content = scanner.useDelimiter("\\Z|\\A").next()
+    get_content: function (http_connection) {
+      var inputStream = http_connection.getInputStream()
+      var scanner = new Scanner(inputStream, "UTF-8")
+      var content = scanner.useDelimiter("\\Z|\\A").next()
 
-            scanner.close()
+      scanner.close()
 
-            return content
-        },
+      return content
+    },
 
-        fetch: (function _fetch() {
-            var http_connection
+    fetch: (function _fetch() {
+      var http_connection
 
-            if (method.toUpperCase() == "POST") {
-                var output;
+      if (method.toUpperCase() == "POST") {
+        var output;
 
-                http_connection = new URL(url).openConnection()
-                http_connection.setRequestMethod(method)
+        http_connection = new URL(url).openConnection()
+        http_connection.setRequestMethod(method)
 
-                Object.keys(properties).forEach(function(prop) {
-                    http_connection.setRequestProperty(prop, properties[prop])
-                })
+        Object.keys(properties).forEach(function (prop) {
+          http_connection.setRequestProperty(prop, properties[prop])
+        })
 
-                http_connection.setDoOutput(true)
-                http_connection.setRequestMethod(method)
+        http_connection.setDoOutput(true)
+        http_connection.setRequestMethod(method)
 
-                try {
-                    (output = http_connection.getOutputStream()).write(params.getBytes(properties.charset))
-                } catch(exception) {
-                    // if (output)
-                    //     output.close()
-                }
-            } else /* if (method.toUpperCase() == "GET") */ {
-                url += "?" + params
-                http_connection = new URL(url).openConnection()
-            }
+        try {
+          (output = http_connection.getOutputStream()).write(params.getBytes(properties.charset))
+        } catch (exception) {
+          // if (output)
+          //     output.close()
+        }
+      } else /* if (method.toUpperCase() == "GET") */ {
+        url += "?" + params
+        http_connection = new URL(url).openConnection()
+      }
 
-            return fluent.get_content(http_connection)
-        }).bind(request)
-    }
+      return fluent.get_content(http_connection)
+    }).bind(request)
+  }
 
-    return fluent
+  return fluent
 }
 
 
@@ -91,15 +90,13 @@ function mount_http_request(method, url, params) {
   * HTTPClient.post("http://localhost:8080/test/hello").fetch()
   */
 var HTTPClient = {
-    get: function (url, params) { return mount_http_request("GET", url, params) },
-    post: function (url, params) { return mount_http_request("POST", url, params) },
-    put: function (url, params) { return mount_http_request("PUT", url, params) },
-    delete: function (url, params) { return mount_http_request("DELETE", url, params) },
+  get: function (url, params) { return mount_http_request("GET", url, params) },
+  post: function (url, params) { return mount_http_request("POST", url, params) },
+  put: function (url, params) { return mount_http_request("PUT", url, params) },
+  delete: function (url, params) { return mount_http_request("DELETE", url, params) },
 }
 
 exports = HTTPClient
-
-
 /*
 print ("\n",
     HTTPClient
