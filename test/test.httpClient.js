7f0a32d798e44836cf4a518e7b19b355a8951370
Ozair Jr <ozair@ivendas.com.br>
2019-11-18 10:43:38 -0300

    Retirado um conflito *merge*

--- test/test.httpClient.js
+++ test/test.httpClient.js
@@ -17,198 +17,172 @@ let httpClient = require('./index')
 //   .fetch()
 
 function exec(describe, it, beforeEach, afterEach, expect, should, assert) {
-  var rs
-  // afterEach(function() { })
-  // beforeEach(function() { })
+    var rs
+    // afterEach(function() { })
+    // beforeEach(function() { })
 
-<<<<<<< HEAD
-  describe('Client HTTP e HTTPS para thrust', function() {
-    describe('Método [GET] Firebase Functions', function() {
-      it('Executando método GET retornando um objeto JSON', function() {
-        var token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjMwNDZhMTU2MzczNjZiNGQ2NGQ5YTVhYmIzMzczMTgyYmE0ZDdjZmIifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZmlyZWZpcnN0LTg3NzgiLCJhdWQiOiJmaXJlZmlyc3QtODc3OCIsImF1dGhfdGltZSI6MTUyNTAwNTEzMSwidXNlcl9pZCI6IjU2eVIwZGRMRW9Ua2FYWmRnTTY0cmVScnBadzEiLCJzdWIiOiI1NnlSMGRkTEVvVGthWFpkZ002NHJlUnJwWncxIiwiaWF0IjoxNTI1MDA5MjIyLCJleHAiOjE1MjUwMTI4MjIsImVtYWlsIjoibmVyeUBzb2Z0Ym94LmNvbS5iciIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJuZXJ5QHNvZnRib3guY29tLmJyIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.t_Q0ZbMTPUxJTjpzIV9fylMuv0s0kRmHd3nUfMQ86z668iY4kf95-S6_5y-JUN2vb71kBaSmBCzakKnP8aYgcLTRw40OZPN_v7zR0GaQF_i3CqYDWIFxd6nYpH_hK5Ma69ukLzk9-8O5JL9stOrZpp_eBwtAu67SZUnrsvVTZ_jse7EGspNkQqZOFQP_A-tgcGVTOlww16PzMKW83DMUGXxANehAZdpfVNcGCG7_eI12bzbP14eCnmnp3ljtYI2iEXg7mhw3anVop4THaaGvwjj3y3z7mCI9CFkrxTjs_PeOcK7B4-0q0_3qpKlH2pmtJG6dqDvSqBCkt381WzTxig'
-        rs = httpClient.get('https://us-central1-firefirst-8778.cloudfunctions.net/crud/777')
-          .headers({
-            'user-agent': 'thrustBot-http-client/1.3.0',
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer ' + token
-          })
-          .fetch()
+    describe('Client HTTP e HTTPS para thrust', function() {
+        describe('Método [GET] Firebase Functions', function() {
+            it('Executando método GET retornando um objeto JSON', function() {
+                var token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjMwNDZhMTU2MzczNjZiNGQ2NGQ5YTVhYmIzMzczMTgyYmE0ZDdjZmIifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZmlyZWZpcnN0LTg3NzgiLCJhdWQiOiJmaXJlZmlyc3QtODc3OCIsImF1dGhfdGltZSI6MTUyNTAwNTEzMSwidXNlcl9pZCI6IjU2eVIwZGRMRW9Ua2FYWmRnTTY0cmVScnBadzEiLCJzdWIiOiI1NnlSMGRkTEVvVGthWFpkZ002NHJlUnJwWncxIiwiaWF0IjoxNTI1MDA5MjIyLCJleHAiOjE1MjUwMTI4MjIsImVtYWlsIjoibmVyeUBzb2Z0Ym94LmNvbS5iciIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJuZXJ5QHNvZnRib3guY29tLmJyIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.t_Q0ZbMTPUxJTjpzIV9fylMuv0s0kRmHd3nUfMQ86z668iY4kf95-S6_5y-JUN2vb71kBaSmBCzakKnP8aYgcLTRw40OZPN_v7zR0GaQF_i3CqYDWIFxd6nYpH_hK5Ma69ukLzk9-8O5JL9stOrZpp_eBwtAu67SZUnrsvVTZ_jse7EGspNkQqZOFQP_A-tgcGVTOlww16PzMKW83DMUGXxANehAZdpfVNcGCG7_eI12bzbP14eCnmnp3ljtYI2iEXg7mhw3anVop4THaaGvwjj3y3z7mCI9CFkrxTjs_PeOcK7B4-0q0_3qpKlH2pmtJG6dqDvSqBCkt381WzTxig'
+                rs = httpClient.get('https://us-central1-firefirst-8778.cloudfunctions.net/crud/777')
+                    .headers({
+                        'user-agent': 'thrustBot-http-client/1.3.0',
+                        'Content-Type': 'application/json; charset=UTF-8',
+                        'Authorization': 'Bearer ' + token
+                    })
+                    .fetch()
 
-        expect(rs.code).to.equal(200)
-        expect(rs.body).to.be.an('object')
-        expect(rs.body).to.have.own.property('id')
-        expect(rs.body.id).to.equal('777')
-      })
-    })
-    describe('Método [GET]', function() {
-      it('Executando método GET retornando um array de objetos json', function() {
-=======
-  describe('Client HTTP e HTTPS para thrust', function () {
-    describe('Método [GET]', function () {
-      it('GET com retorno 404', function () {
-        rs = httpClient.get('https://postman-echo.com/status/404')
-          .fetch()
-
-        expect(rs.code).to.equal(404)
-      })
-
-      it('GET com retorno 500', function () {
-        rs = httpClient.get('https://postman-echo.com/status/500')
-          .fetch()
-
-        expect(rs.code).to.equal(500)
-      })
-
-      it('Executando método GET retornando um array de objetos json', function () {
->>>>>>> 87338d182d01395a77642522fabed716ff21fe75
-        rs = httpClient.get('https://jsonplaceholder.typicode.com/posts')
-          .charset('UTF-8')
-          .fetch()
-
-        expect(rs.code).to.equal(200)
-        expect(rs.body).to.be.an('array')
-        expect(rs.body.length).to.equal(101)
-      })
-
-      it('Executando método GET retornando um objeto json', function () {
-        rs = httpClient.get('https://jsonplaceholder.typicode.com/posts/1')
-          .charset('UTF-8')
-          .fetch()
-
-        expect(rs.code).to.equal(200)
-        expect(rs.body.id).to.equal(1)
-      })
-
-      it('Método GET utilizando [.headers]', function () {
-
-        rs = httpClient.get('https://postman-echo.com/headers')
-          .headers({
-            'app': 'thrust',
-            'user-agent': 'thrustBot-http-client/1.3.0',
-            'Content-Type': 'application/json; charset=UTF-8'
-          })
-          .fetch()
-
-        expect(rs.code).to.equal(200)
-        expect(rs.body).to.not.equal(undefined)
-        expect(rs.body.headers).to.not.equal(undefined)
-        expect(rs.body.headers.app).to.equal('thrust')
-      })
-
-      it('GET com disable de certificado', function () {
-        rs = httpClient
-          .get('https://postman-echo.com/status/200')
-          .disableCertificateValidation()
-          .fetch()
-
-        expect(rs.code).to.equal(200)
-      })
-    })
-    describe('Método [POST]', function () {
-
-      it('POST com retorno 404', function () {
-        rs = executaEMostraTempo(function () {
-          return httpClient.post('https://postman-echo.com/status/404')
-            .fetch()
-
-          expect(rs.code).to.equal(404)
-        })
-      })
-
-      it('Executando método POST inserindo um objeto json', function () {
-        rs = executaEMostraTempo(function () {
-          return httpClient.post('https://reqres.in/api/users')
-            .property('user-agent', 'thrustBot-http-client/1.3.0')
-            .contentType('application/json')
-            .params({
-              'name': 'thrust',
-              'job': 'platform'
+                expect(rs.code).to.equal(200)
+                expect(rs.body).to.be.an('object')
+                expect(rs.body).to.have.own.property('id')
+                expect(rs.body.id).to.equal('777')
             })
-            .fetch()
         })
+        describe('Método [GET]', function() {
+            it('Executando método GET retornando um array de objetos json', function() {
+                rs = httpClient.get('https://jsonplaceholder.typicode.com/posts')
+                    .charset('UTF-8')
+                    .fetch()
 
-        expect(rs.code).to.equal(201)
-        expect(rs.body).to.have.own.property('id')
-        expect(rs.body.id).to.not.equal(undefined)
-        expect(rs.body).to.include({ 'name': 'thrust', 'job': 'platform' })
-      })
-
-      it('Executando método POST (site 2) inserindo um objeto json', function () {
-        rs = httpClient.post('https://jsonplaceholder.typicode.com/posts')
-          .property('user-agent', 'thrustBot-http-client/1.3.0')
-          .contentType('application/json; charset=UTF-8')
-          .params({
-            'title': 'foo',
-            'body': 'bar',
-            'userId': 1
-          })
-          .fetch()
-
-        expect(rs.code).to.equal(201)
-        expect(rs.body).to.have.own.property('id')
-        expect(rs.body).to.have.own.property('userId')
-        expect(rs.body.id).to.not.equal(undefined)
-        expect(rs.body).to.include({ 'title': 'foo', 'body': 'bar' })
-      })
-
-      it('Método POST utilizando [.headers]', function () {
-        rs = httpClient.post('https://jsonplaceholder.typicode.com/posts')
-          .headers({
-            'origin': 'chrome-extension://aejoelaoggembcahagimdiliamlcdmfm',
-            // 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
-            'user-agent': 'thrustBot-http-client/1.3.0',
-            'Content-Type': 'application/json; charset=UTF-8'
-          })
-          .params({
-            'title': 'foo',
-            'body': 'bar',
-            'userId': 1
-          })
-          .fetch()
-
-        expect(rs.code).to.equal(201)
-        expect(rs.body).to.have.own.property('id')
-        expect(rs.body).to.have.own.property('userId')
-        expect(rs.body.id).to.not.equal(undefined)
-        expect(rs.body).to.include({ 'title': 'foo', 'body': 'bar' })
-      })
-
-      it('Método POST sem body no retorno', function () {
-        rs = executaEMostraTempo(function () {
-          return httpClient.post('https://mockbin.org/echo')
-            .headers({
-              'Accept': 'application/json'
+                expect(rs.code).to.equal(200)
+                expect(rs.body).to.be.an('array')
+                expect(rs.body.length).to.equal(101)
             })
-            .fetch()
-        })
 
-        expect(rs.code).to.equal(200)
-        expect(rs.body).to.equal(undefined)
-      })
+            it('Executando método GET retornando um objeto json', function() {
+                rs = httpClient.get('https://jsonplaceholder.typicode.com/posts/1')
+                    .charset('UTF-8')
+                    .fetch()
+
+                expect(rs.code).to.equal(200)
+                expect(rs.body.id).to.equal(1)
+            })
+
+            it('Método GET utilizando [.headers]', function() {
+                rs = httpClient.get('https://postman-echo.com/headers')
+                    .headers({
+                        'app': 'thrust',
+                        'user-agent': 'thrustBot-http-client/1.3.0',
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    })
+                    .fetch()
+
+                expect(rs.code).to.equal(200)
+                expect(rs.body).to.not.equal(undefined)
+                expect(rs.body.headers).to.not.equal(undefined)
+                expect(rs.body.headers.app).to.equal('thrust')
+            })
+
+            it('GET com disable de certificado', function() {
+                rs = httpClient
+                    .get('https://postman-echo.com/status/200')
+                    .disableCertificateValidation()
+                    .fetch()
+
+                expect(rs.code).to.equal(200)
+            })
+        })
+        describe('Método [POST]', function() {
+            it('POST com retorno 404', function() {
+                rs = executaEMostraTempo(function() {
+                    return httpClient.post('https://postman-echo.com/status/404')
+                        .fetch()
+
+                    expect(rs.code).to.equal(404)
+                })
+            })
+
+            it('Executando método POST inserindo um objeto json', function() {
+                rs = executaEMostraTempo(function() {
+                    return httpClient.post('https://reqres.in/api/users')
+                        .property('user-agent', 'thrustBot-http-client/1.3.0')
+                        .contentType('application/json')
+                        .params({
+                            'name': 'thrust',
+                            'job': 'platform'
+                        })
+                        .fetch()
+                })
+
+                expect(rs.code).to.equal(201)
+                expect(rs.body).to.have.own.property('id')
+                expect(rs.body.id).to.not.equal(undefined)
+                expect(rs.body).to.include({ 'name': 'thrust', 'job': 'platform' })
+            })
+
+            it('Executando método POST (site 2) inserindo um objeto json', function() {
+                rs = httpClient.post('https://jsonplaceholder.typicode.com/posts')
+                    .property('user-agent', 'thrustBot-http-client/1.3.0')
+                    .contentType('application/json; charset=UTF-8')
+                    .params({
+                        'title': 'foo',
+                        'body': 'bar',
+                        'userId': 1
+                    })
+                    .fetch()
+
+                expect(rs.code).to.equal(201)
+                expect(rs.body).to.have.own.property('id')
+                expect(rs.body).to.have.own.property('userId')
+                expect(rs.body.id).to.not.equal(undefined)
+                expect(rs.body).to.include({ 'title': 'foo', 'body': 'bar' })
+            })
+
+            it('Método POST utilizando [.headers]', function() {
+                rs = httpClient.post('https://jsonplaceholder.typicode.com/posts')
+                    .headers({
+                        'origin': 'chrome-extension://aejoelaoggembcahagimdiliamlcdmfm',
+                        // 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+                        'user-agent': 'thrustBot-http-client/1.3.0',
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    })
+                    .params({
+                        'title': 'foo',
+                        'body': 'bar',
+                        'userId': 1
+                    })
+                    .fetch()
+
+                expect(rs.code).to.equal(201)
+                expect(rs.body).to.have.own.property('id')
+                expect(rs.body).to.have.own.property('userId')
+                expect(rs.body.id).to.not.equal(undefined)
+                expect(rs.body).to.include({ 'title': 'foo', 'body': 'bar' })
+            })
+
+            it('Método POST sem body no retorno', function() {
+                rs = executaEMostraTempo(function() {
+                    return httpClient.post('https://mockbin.org/echo')
+                        .headers({
+                            'Accept': 'application/json'
+                        })
+                        .fetch()
+                })
+
+                expect(rs.code).to.equal(200)
+                expect(rs.body).to.equal(undefined)
+            })
+        })
     })
-  })
 }
 
 function executaEMostraTempo(fn) {
-  var di = new Date().getTime()
-  var result = fn()
-  var df = new Date().getTime()
+    var di = new Date().getTime()
+    fn()
+    var df = new Date().getTime()
 
-  print('\tTempo de execução:', (df - di), 'ms.')
+    print('\tTempo de execução:', (df - di), 'ms.')
 
-<<<<<<< HEAD
-res.failure.forEach(function(fail) {
-  print('[' + fail.scenario + '] =>', fail.execption)
-  var i = 0
-  if (fail.execption.printStackTrace && i === 0) {
-    fail.execption.printStackTrace()
-    i++
-  }
-})
-=======
-  return result
+    var res = majesty.run(exec)
+    res.failure.forEach(function(fail) {
+        print('[' + fail.scenario + '] =>', fail.execption)
+        var i = 0
+        if (fail.execption.printStackTrace && i === 0) {
+            fail.execption.printStackTrace()
+            i++
+        }
+    })
+
+    exit(res.failure.length)
 }
->>>>>>> 87338d182d01395a77642522fabed716ff21fe75
-
-var res = majesty.run(exec)
-exit(res.failure.length)
